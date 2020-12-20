b6a2de93c2dc5e3ff8cdc748cfd31a84766e4fa4
Flide <76139310+Flllide@users.noreply.github.com>
2020-12-20 03:22:41 +0200

    ConstructorWindows.js

--- ConstructorWindows.js
+++ ConstructorWindows.js
@@ -1,36 +1,45 @@
+var Directory = document.querySelector('.Directory'); 
+
+
+
+
+
 function Window(ContainerId, number){
   this._Constructor= new Constructor(ContainerId, number);
   this._Eventer = new Eventer(ContainerId);
 }
-var Directory = document.querySelector('body> .Directory');
-
 
 function Constructor(ContainerId, number){
   this._render(ContainerId, number);
 };
 
 Constructor.prototype = {
-   _render: function(ContainerId, number){
-     if(Directory=== null){
-       this._Body.innerHTML +=`<div class="Directory"></div>`;
-       Directory = document.querySelector('body> .Directory');
-       Log("Создана Директория");
-       this._CreaterWindow(ContainerId, number);
-       
-     }else{
-       this._CreaterWindow(ContainerId, number);
-     }
-   },
    _Body: document.querySelector('body'),
-   _Directory: document.querySelector('body> .Directory'), 
+   
+   _render: function(ContainerId, number){
+      if(Directory=== null){
+        var elementDirectory = document.createElement("div");
+        elementDirectory.className = "Directory";
+        this._Body.appendChild(elementDirectory);
+        Directory = document.querySelector('.Directory'); 
+        Log("Создана Директория");
+        this._CreaterWindow(ContainerId, number);
+       }else{
+         this._CreaterWindow(ContainerId, number);
+       }
+   },
    _CreaterWindow: function(ContainerId, number){
-    Directory.innerHTML +=`
-      <div class="window" id= "${ContainerId}">${ContainerId} <br/>
-        <input class="clicker" id="${ContainerId+"Clicker"}" type="button" value="Кнопка" onclick='Windows[${number}]._Eventer._Click("${ContainerId}")'>
-        <div class="display" id="${ContainerId+"Display"}">Результат</div> 
-      </div> 
+     var elementWindow = document.createElement("div");
+     elementWindow.className = "window";
+     elementWindow.id =ContainerId;
+     elementWindow.innerHTML = `
+        <div class="window" id= "${ContainerId}">${ContainerId} <br/>
+          <input class="clicker" id="${ContainerId+"Clicker"}" type="button" value="Кнопка" >
+          <div class="display" id="${ContainerId+"Display"}">Результат</div> 
+        </div> 
     `;
-    Log("создано окно") 
+     Directory.appendChild(elementWindow);
+     Log(`создан элемент ${ContainerId}`) 
    },
 }
 
@@ -47,13 +56,23 @@ Eventer.prototype = {
 		this._ButtonWindow = document.querySelector(`#${ContainerId}Clicker`);
 		this._DisplayWindow = document.querySelector(`#${ContainerId}Display`);
 		self = this
-			//this._ButtonWindow.addEventListener("click", function(){self._Click(ContainerId)});
-	        // Windows[0]._Eventer._Click(ContainerId)
-	    Log(ContainerId) 
+			this._ButtonWindow.addEventListener("click", function(){self._Click(ContainerId)});
+	       
   },
   
   _Click: function(ContainerId){
-    var lpt = document.querySelector(`#${ContainerId}Display`); 
-    lpt. innerHTML= 658987;
+    var lpt = document.querySelector(`#${ContainerId}Display`);
+    var yserData = new Date();
+
+    lpt. innerHTML= `ваш ip: ${IP} <br/> ваше время: ${yserData}`;
   },
 }
+/*
+    "Год сегодня - " + date.getFullYear() + br +
+     "месяц сегодня - " + date.getMonth() + br +
+     "день сегодня - " + date.getDate() + br +
+     "часов сейчас - " + date.getHours() + br +
+     "минут - " + date.getMinutes() + br +
+     "cекунд - " + date.getSeconds() + br ;
+*/
+	
